67ba3522bfb23cbf7c0c70340222afac0afa92db
diogovieira63 <a93251@alunos.uminho.pt>
2022-10-25 17:21:30 +0100

    update

--- Makefile
+++ Makefile
@@ -2,7 +2,8 @@ CC = gcc
 BIN= bin/
 SRC = src/
 EXEC = k_means
-CFLAGS = -O2 -lm
+CFLAGS = -O2 
+SFLAGS =  -g -fno-omit-frame-pointer
 .DEFAULT_GOAL = k_means
 k_means: $(SRC)k_means.c 
 	$(CC) $(CFLAGS) $(SRC)k_means.c  -o $(BIN)$(EXEC)
@@ -10,3 +11,9 @@ clean:
 	rm -r bin/*
 run: 
 	./$(BIN)$(EXEC)
+
+assembly:
+	$(CC) $(CFLAGS) $(SFLAGS) -S $(SRC)k_means.c  -o $(EXEC)
+
+perf:
+	 perf stat -e instructions,cycles,L1-dcache-load-misses  $(BIN)$(EXEC)
--- src/k_means.c
+++ src/k_means.c
@@ -4,96 +4,171 @@
 
 
 #define N 10000000
-//#define N 100
 
 #define X(i) (i*2)
 #define Y(i) (i*2+1)
 #define K 4
 
-float *vector, *centroid, *sum,**cluster;
 
-int tempSize[K],size[K];
+float *vector, *centroid,*cluster;
+int size[K] = {0};
+
 
 void alloc() {
-    vector = (float *) malloc(N*2*sizeof(float));
-    centroid = (float *) malloc(K*2*sizeof(float));
-    sum = (float *) malloc(K*2*sizeof(float));
-
-
-    cluster = (float **)malloc(K * sizeof(float *));
-    for (int i = 0; i < K;i++){
-        cluster[i] = (float *) malloc(N*2*sizeof(float));
-    }
+    vector =  (float *) malloc(N*2*sizeof(float));
+    cluster = (float *) malloc(N *sizeof(float));
+    centroid =(float *) malloc(K*2*sizeof(float));
+    //sum =     (float *) malloc(K*2*sizeof(float));
 }
 
 
 void inicializa() {
     srand(10);
-
     // initialize vector with N values (x,y)
     for(int i = 0; i < N; i++) {
         vector[X(i)] = (float) rand() / RAND_MAX;
         vector[Y(i)] = (float) rand() / RAND_MAX;
+        cluster[i] = 0;
     }
     // initialize cluster values
     for(int i = 0; i < K; i++) {
         centroid[X(i)]  = vector[X(i)];
         centroid[Y(i)]= vector[Y(i)];
-        size[i] = 0;
     }
 }
 
 
-float distance (float *vec, float *cl){
-    return sqrt(pow(cl[0] - vec[0],2) + pow(cl[1] - vec[1],2));
+
+float distance1 (float x,float y, float *cl){
+    return ((cl[0] - x) * (cl[0] - x)  + (cl[1] - y) * (cl[1] - y));
 }
 
+//float distance (float *vec, float *cl){
+//    return sqrt(__builtin_powi(cl[0] - vec[0],2)  + __builtin_powi(cl[1] - vec[1],2));
+//}
+/*
 void addToCluster(int numCluster, float x, float y){
-    int n = tempSize[numCluster]++;
-    cluster[numCluster][X(n)]= x;
-    cluster[numCluster][Y(n)]= y; 
-    sum[X(numCluster)] += x; 
-    sum[Y(numCluster)] += y; 
+    //cluster[numCluster][X(n)]= x;
+    //cluster[numCluster][Y(n)]= y; 
+    //sum[X(numCluster)] += x; 
+    //sum[Y(numCluster)] += y; 
 }
+*/
 
 void showResult(int it){
-
     printf("N = %d, K = %d\n",N,K);
     for (int i = 0;i<K ;i++){
         printf("Center: (%.3f,%.3f) : Size: %d\n",centroid[X(i)],centroid[Y(i)],size[i]);
     }
     printf("Iterations: %d\n",it);
 }
+/*
+void calculate_centroids(){
+
+    //for (int u = 0; u < K;u++){
+    //    centroid[X(u)] = sum[X(u)]/size[u];
+    //    centroid[Y(u)] = sum[Y(u)]/size[u];
+    //    sum[X(u)] = 0;
+    //    sum[Y(u)] = 0;
+    //    size[u] = 0;
+    //}
+    float sum[K*2];
+    for(int i =0; i < K;i++)sum[X(i)] = 0;
+    for(int i = 0; i < N;i++){
+        int index = cluster[i];
+        sum[X(index)] += vector[X(i)];
+        sum[Y(index)] += vector[Y(i)];
+    }
+    for (int i = 0; i < K; i++){
+        centroid[X(i)] = sum[X(i)]/size[i];
+        centroid[Y(i)] = sum[Y(i)]/size[i];
+    }
+
+}
+
+int calculate_centroids2(){
+    int changed = 0;
+    for (int u = 0; u < K;u++){
+        float sumX = 0, sumY =0, sizeCl = tempSize[u];
+        for (int j = 0;j < sizeCl;j++){
+            sumX += cluster[u][X(j)];
+            sumY += cluster[u][Y(j)];
+        }
+        centroid[X(u)] = sumX/sizeCl;
+        centroid[Y(u)] = sumY/sizeCl;
+        //printf("%d\n",tempSize[u]);
+        sum[X(u)] = 0;
+        sum[Y(u)] = 0;
+        if (tempSize[u] != size[u]) {
+            size[u] = tempSize[u];
+            changed = 1;
+        }
+        tempSize[u] = 0;
+    }
+    return changed;
+}
+
+
+void atrib_points(){
+    int changed = 0;
+    for (int i = 0; i < N ;i++){
+        float lowest = 1.5f;
+        int index_low = -1;
+        for (int k = 0; k < K;k++){
+            float dist = distance1(&vector[X(i)],&centroid[X(k)]);
+            if (dist < lowest){
+                lowest = dist;
+                index_low = k;
+            }
+        }
+        if (index_low != cluster[i]){
+            cluster[i]=index_low;
+            changed = 1;
+        }
+        size[index_low]++;
+        //addToCluster(index_low,vector[X(i)],vector[Y(i)]);
+    }
+    return changed;
+}
+*/
 
 void k_means(){
     int changed = 1, iterations = 0;
     while (changed){
+        changed = 0;
+        int tempSize[K] = {0};
+        float sum[K*2] = {0};
         for (int i = 0; i < N ;i++){
-            float lowest = distance(&vector[X(i)],centroid);
+            float x  = vector[X(i)], y = vector[Y(i)];
+            float lowest = distance1(x,y,&centroid[X(0)]);
             int index_low = 0;
             for (int k = 1; k < K;k++){
-                float dist = distance(&vector[X(i)],&centroid[X(k)]);
+                float dist = distance1(x,y,&centroid[X(k)]);
                 if (dist < lowest){
                     lowest = dist;
                     index_low = k;
                 }
             }
-            addToCluster(index_low,vector[X(i)],vector[Y(i)]);
-        }
-        changed = 0;
-        for (int u = 0; u < K;u++){
-            centroid[X(u)] = sum[X(u)]/tempSize[u];
-            centroid[Y(u)] = sum[Y(u)]/tempSize[u];
-            sum[X(u)] = 0;
-            sum[Y(u)] = 0;
-            if (tempSize[u] != size[u]) {
-                size[u] = tempSize[u];
+            //printf("%d | %d\n",index_low,cluster[i]);
+            if (index_low != cluster[i]){
+                cluster[i]=index_low;
                 changed = 1;
             }
-            tempSize[u] = 0;
+            sum[X(index_low)] += vector[X(i)]; 
+            sum[Y(index_low)] += vector[Y(i)]; 
+            tempSize[index_low]++;
         }
+        for (int i = 0; i < K; i++){
+            size[i] = tempSize[i];
+            centroid[X(i)] = sum[X(i)]/size[i];
+            centroid[Y(i)] = sum[Y(i)]/size[i];
+        }
+
+
+       //calculate_centroids();
        iterations++;
     }
+
     showResult(iterations - 1);
 }
 
