8ace216d82e3db17815323b2b4485f5352c03fbf
diogovieira63 <a93251@alunos.uminho.pt>
2022-11-18 18:52:13 +0000

    final TP2

--- Makefile
+++ Makefile
@@ -2,19 +2,32 @@ CC = gcc
 BIN= bin/
 SRC = src/
 EXEC = k_means
-CFLAGS= -O2 -Wall -ftree-vectorize -mavx 
+CFLAGS= -O3 -Wall -ftree-vectorize -mavx -fopenmp
 SFLAGS =  -g -fno-omit-frame-pointer
 .DEFAULT_GOAL = k_means
+THREADS = 40
+
+
+$(shell mkdir -p $(BIN))
 
 k_means: $(SRC)k_means.c 
 	$(CC) $(CFLAGS) $(SRC)k_means.c  -o $(BIN)$(EXEC)
 clean: 
 	rm -r bin/*
-run: 
-	./$(BIN)$(EXEC)
 
+
+perfseq:
+	 perf stat -e instructions,cycles,L1-dcache-load-misses -r 5 $(BIN)$(EXEC) 10000000 $(CP_CLUSTERS)
+
+perfpar:
+	 perf stat -e instructions,cycles,L1-dcache-load-misses -r 5 $(BIN)$(EXEC) 10000000 $(CP_CLUSTERS) $(THREADS)
+
+runseq:
+	./$(BIN)$(EXEC) 10000000 $(CP_CLUSTERS)
+	
+runpar:
+	./$(BIN)$(EXEC) 10000000 $(CP_CLUSTERS) $(THREADS)
+
+	
 assembly:
 	$(CC) $(CFLAGS) $(SFLAGS) -S $(SRC)k_means.c  -o $(EXEC)
-
-perf:
-	 perf stat -e instructions,cycles,L1-dcache-load-misses  $(BIN)$(EXEC)
--- src/k_means.c
+++ src/k_means.c
@@ -1,39 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
+#include <string.h>
 
 
-#define N 10000000
+#define MAX_IT 20
 
 #define X(i) (i*2)
 #define Y(i) (i*2+1)
-#define K 4
 
 
-float vector[N*2]   __attribute__((aligned (32)));
+int N , K, NumThreads;
+
+float *vectorX;//[N] __attribute__((aligned (32)));
+float *vectorY;//[N] __attribute__((aligned (32)));
+
 float  *centroid;//,*vector;
-int size[K] = {0},*cluster;
+int *size,*cluster;
 
 //Function to allocate memory espace
 void alloc() {
     //vector =  (float *) malloc(N*2*sizeof(float));
+    vectorX = aligned_alloc(32,N * sizeof(float));
+    vectorY = aligned_alloc(32,N * sizeof(float));
+
     cluster = (int *)   malloc(N *sizeof(int));
     centroid =(float *) malloc(K*2*sizeof(float));
+    size = (int *) malloc(K*sizeof(int));
 }
 
 //Function to initialize the clusters and so the algorithm
 void inicializa() {
     srand(10);
+    
     // initialize vector with N values (x,y)
     for(int i = 0; i < N; i++) {
-        vector[X(i)] = (float) rand() / RAND_MAX;
-        vector[Y(i)] = (float) rand() / RAND_MAX;
+        vectorX[i] = (float) rand() / RAND_MAX;
+        vectorY[i] = (float) rand() / RAND_MAX;
         //cluster[i] = 0;
     }
     // initialize cluster values
+    #pragma omp parallel for num_threads(NumThreads)
     for(int i = 0; i < K; i++) {
-        centroid[X(i)] = vector[X(i)];
-        centroid[Y(i)] = vector[Y(i)];
+        centroid[X(i)] = vectorX[i];
+        centroid[Y(i)] = vectorY[i];
     }
 }
 
@@ -51,53 +62,89 @@ void showResult(int it){
     printf("Iterations: %d\n",it);
 }
 
-//Main Function of the program to run the k-means algorithm
-int k_means(){
-    int changed = 1, iterations = 0;
-    while (changed){
-        changed = 0;
-        int tempSize[K] = {0};
-        float sum[K*2] = {0};
-        for (int i = 0; i < N ;i++){ 
-            float lowest = distance(vector[X(i)],vector[Y(i)],&centroid[X(0)]);
-            int index_low = 0;
-            for (int k = 1; k < K;k++){
-                float dist = distance(vector[X(i)],vector[Y(i)],&centroid[X(k)]);
+
+void assignCluster(){
+    #pragma omp parallel for num_threads(NumThreads)
+    for (int i = 0; i < N ;i++){ 
+        float lowest = distance(vectorX[(i)],vectorY[(i)],&centroid[X(0)]);
+        int index_low = 0;
+        for (int k = 1; k < K;k++){
+            float dist = distance(vectorX[(i)],vectorY[(i)],&centroid[X(k)]);
                 if (dist < lowest){
                     lowest = dist;
                     index_low = k;
                 }
-            }
-            // Check if a value changed cluster
-            if (index_low != cluster[i]){
-                cluster[i]=index_low;
-                changed = 1;
-            }
-            //sum[X(index_low)] += vector[X(i)]; 
-            //sum[Y(index_low)] += vector[Y(i)]; 
-            //tempSize[index_low]++;
         }
-        for (int i = 0;i < N;i++){
-            int index_low = cluster[i];
-            //sum values x,y for centroid calculation
-            sum[X(index_low)] += vector[X(i)]; 
-            sum[Y(index_low)] += vector[Y(i)]; 
-            tempSize[index_low]++;
-        }
-        for (int i = 0; i < K; i++){
-            size[i] = tempSize[i];
-            centroid[X(i)] = sum[X(i)]/tempSize[i];
-            centroid[Y(i)] = sum[Y(i)]/tempSize[i];
-        }
-       iterations++;
+        cluster[i]=index_low;
     }
-    return iterations-1;
 }
 
-int main (){
-    alloc();
-    inicializa();
-    int iterations = k_means();
-    showResult(iterations);
+/*
+void assignClusterCollapse(){
+    float lowest = 10;
+    #pragma omp parallel for num_threads(NumThreads) collapse(2) firstprivate(lowest)
+    for (int i = 0; i < N ;i++){ 
+        for (int k = 0; k < K;k++){
+            float dist = distance(vectorX[(i)],vectorY[(i)],&centroid[X(k)]);
+            if (dist < lowest){
+                lowest = dist;
+                cluster[i] = k;
+            }
+            if(k == K-1) lowest =10;
+        }
+    }
+}
+*/
+
+
+void calculateCentroid(){
+    int tempSize[K];
+    float sumX[K];
+    float sumY[K];
+    memset( tempSize, 0,K*sizeof(int) );
+    memset( sumX, 0,K*sizeof(float) );
+    memset( sumY, 0,K*sizeof(float) );
+
+    #pragma omp parallel for num_threads(NumThreads) reduction(+:sumX[:K],sumY[:K],tempSize[:K])
+    for (int i = 0;i < N;i++){
+        int index_low = cluster[i];
+        sumX[(index_low)] += vectorX[(i)]; 
+        sumY[(index_low)] += vectorY[(i)]; 
+        tempSize[index_low]++;
+    }
+
+    #pragma omp parallel for num_threads(NumThreads)
+    for (int i = 0; i < K; i++){
+        size[i] = tempSize[i];
+        centroid[X(i)] = sumX[(i)]/tempSize[i];
+        centroid[Y(i)] = sumY[(i)]/tempSize[i];
+    }
+}
+
+//Main Function of the program to run the k-means algorithm
+int k_means(){ 
+    for (int u = 0; u <= MAX_IT;u++){
+        assignCluster();
+        calculateCentroid();
+    }
+    return MAX_IT;
+}
+
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+int main (int argc, char ** argv){
+    if (argc <= 4){
+        N = atoi(argv[1]);
+        K = atoi(argv[2]);
+        NumThreads = argc == 3 ? 1 : MIN(atoi(argv[3]),omp_get_max_threads()); 
+
+        alloc();
+        inicializa();
+        int iterations = k_means();
+        showResult(iterations);
+    }
+    else {
+        printf("Nº de argumentos inválido.");
+    }
 
 }
